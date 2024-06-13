50deffd3542f689983a0c728ca77ebbaabf0e605
hb9901 <hb9901@gmail.com>
2024-06-14 00:53:43 +0900

    fix: private 로그인 여부 수정

--- src/routes/Private.jsx
+++ src/routes/Private.jsx
@@ -6,7 +6,7 @@ function Private() {
   const { isAuthenticated } = useUser();
   const accessToken = localStorage.getItem("accessToken");
 
-  if (!isAuthenticated || !accessToken) {
+  if (!isAuthenticated && !accessToken) {
     alert("로그인이 필요한 기능입니다.");
     return <Navigate to="/login" />;
   }
